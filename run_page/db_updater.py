cd5602568f3631f0b675e9a6ac8603f60c2a7e1a
wangsiji <wangsiji@local>
2026-09-22 17:03:42 +0800

    running_page: 思己跑步站(高驰数据源)

---
+++ run_page/db_updater.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+from config import GPX_FOLDER, JSON_FILE, SQL_FILE
+from generator.db import Activity, init_db
+from sqlalchemy import text
+
+from utils import make_activities_file
+
+
+def add_column_elevation_gain(session):
+    # check if column elevation_gain is already added
+    # if not add it to the db
+    try:
+        session.query(Activity).first()
+        print("column elevation_gain already added, skipping")
+    except sqlalchemy.exc.OperationalError:
+        sql_statement = 'alter TABLE "activities" add column elevation_gain Float after average_heartrate'
+        session.execute(text(sql_statement))
+        print("column elevation_gain added successfully")
+
+
+if __name__ == "__main__":
+    session = init_db(SQL_FILE)
+    add_column_elevation_gain(session)
+    # regenerate activities
+    make_activities_file(SQL_FILE, GPX_FOLDER, JSON_FILE)
